4a0f9f85759d81289a819ba73d84e0bd7d1a5ed6
arun <arun2k07@gmail.com>
2025-06-09 16:07:57 +0530

    html

---
+++ demo.py
@@ -0,0 +1,26 @@
+"""a=10
+b=20
+c=5
+if a>b:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+else:
+    print("c is greater")
+for i in range(1,16,2):
+        print(i)
+def add():
+    print(10+20) 
+add()
+list = [1,2,3,4,5];
+for i in list:
+    print(i)
+tuple=(6,7,8,9,10);
+for i in tuple:
+    print(i)
+print(type(tuple))
+dictionary = {"name":"arunesh","pass":"arun"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])"""
